b1eab36aa16ecf1e550154979ee9865cb72349f9
weixin_45501559 <weixin_45501559@noreply.gitcode.com>
2025-08-25 16:12:41 +0800

    commit Provider 状态管理

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:code01/Routes/RouteManager.dart';
 import 'package:code01/datas/home_banner_data.dart';
 import 'package:code01/pages/home/home_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'package:provider/provider.dart';
+
+import '../../datas/home_list_data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,68 +19,95 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<BannerItemData>? bannerList;
+  HomeViewModel viewModel = HomeViewModel();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("首页"),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-          // SingleChildScrollView 实现整体滑动，需要知道子组件整体的高度
-          child: SingleChildScrollView(
-        child: Column(children: [
-          // 轮播图
-          _banner(),
-          // ListView 列表
-          ListView.builder(
-              shrinkWrap: true,
-              // 禁用 ListView 滑动事件
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, newIndex) {
-                return _ListItemWidget();
-              },
-              itemCount: 20)
-        ]),
-      )),
-    );
+    return ChangeNotifierProvider<HomeViewModel>(
+        create: (context) {
+          print("build, viewModel instance: $viewModel");
+          // 或者打印具体字段
+          print("build, bannerList length: ${viewModel.bannerList?.length}");
+          print(
+              "build, homeArticleList length: ${viewModel.homeArticleList?.length}");
+          return viewModel;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("首页"),
+            centerTitle: true,
+          ),
+          body: SafeArea(
+              // SingleChildScrollView 实现整体滑动，需要知道子组件整体的高度
+              child: SingleChildScrollView(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                // ✅ 关键3：让子项横向撑满
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // 轮播图
+                  _banner(),
+                  // ListView 列表
+                  _listView()
+                ]),
+          )),
+        ));
   }
 
   Widget _banner() {
-    return Container(
-      height: 200,
-      //decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.all(Radius.circular(10))),
-      child: Swiper(
-        itemCount: bannerList?.length ?? 0,
-        indicatorLayout: PageIndicatorLayout.SCALE,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              //color: Colors.lightBlue,
-              borderRadius: BorderRadius.circular(8),
+    // 接收Provider
+    return Consumer<HomeViewModel>(builder: (context, vm, child) {
+      return Container(
+        height: 200,
+        //decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Swiper(
+          itemCount: vm.bannerList?.length ?? 0,
+          indicatorLayout: PageIndicatorLayout.SCALE,
+          itemBuilder: (context, index) {
+            return Container(
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  //color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Image.network(vm.bannerList?[index].imagePath ?? "",
+                    fit: BoxFit.fill));
+          },
+          autoplay: true,
+          // 可选：添加分页指示器
+          pagination: const SwiperPagination(
+            builder: DotSwiperPaginationBuilder(
+              activeColor: Colors.white,
+              color: Colors.grey,
+              size: 15,
+              activeSize: 20,
             ),
-            child: Image.network(bannerList?[index].imagePath ?? "",fit: BoxFit.fill)
-          );
-        },
-        autoplay: true,
-        // 可选：添加分页指示器
-        pagination: const SwiperPagination(
-          builder: DotSwiperPaginationBuilder(
-            activeColor: Colors.white,
-            color: Colors.grey,
-            size: 15,
-            activeSize: 20,
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 
-  Widget _ListItemWidget() {
+  Widget _listView() {
+    return Consumer<HomeViewModel>(builder: (context, vm, child) {
+      return ListView.builder(
+          shrinkWrap: true,
+          // 禁用 ListView 滑动事件
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, newIndex) {
+            return _listItemWidget(vm.homeArticleList?[newIndex]);
+          },
+          itemCount: vm.homeArticleList?.length);
+    });
+  }
+
+  Widget _listItemWidget(HomeItemData? item) {
+    var name;
+    if(item?.author?.isEmpty == true){
+      name = item?.shareUser ?? "";
+    }else{
+      name = item?.author ?? "";
+    }
     return GestureDetector(
         onTap: () {
           // 跳转到文章页面
@@ -90,9 +122,11 @@ class _HomePageState extends State<HomePage> {
               border: Border.all(color: Colors.black26, width: 2),
               borderRadius: BorderRadius.all(Radius.circular(10))),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
+                  // 头像
                   Container(
                       margin: EdgeInsets.only(left: 10, top: 8, right: 15),
                       height: 40,
@@ -103,11 +137,11 @@ class _HomePageState extends State<HomePage> {
                           fit: BoxFit.fill,
                         ),
                       )),
-                  Text("作者",
+                  Text(name,
                       style:
                           TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                   Expanded(child: SizedBox()),
-                  Text("2025-8-22",
+                  Text(item?.niceShareDate ?? "",
                       style:
                           TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                   SizedBox(width: 15),
@@ -122,10 +156,12 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               Container(
-                margin: EdgeInsets.only(top: 10, bottom: 5, left: 8, right: 8),
+                margin: EdgeInsets.only(top: 12, bottom: 26, left: 8, right: 8),
                 child: Text(
-                  "内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容",
+                  item?.title ?? "",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  maxLines: 1, // 只显示一行
+                  overflow: TextOverflow.ellipsis, // 超出部分显示 ...
                 ),
               ),
               Container(
@@ -133,7 +169,7 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   children: [
                     Text(
-                      "分类",
+                      item?.chapterName ?? "",
                       style: TextStyle(
                           color: Colors.green,
                           fontSize: 14,
@@ -156,14 +192,13 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    getBannerData();
-  }
-
-  void getBannerData() async{
-    bannerList = await HomeViewModel.getBanner();
-    setState(() {
-      //虽然 setState 里是空的，但它触发了 StatefulWidget 的 build 方法重新执行，
-      // 从而让 bannerList 的新值反映到界面上。
-    });
+    viewModel.initDio();
+    viewModel.getBanner();
+    viewModel.getArticle();
   }
 }
+
+//虽然 setState 里是空的，但它触发了 StatefulWidget 的 build 方法重新执行，
+// 从而让 bannerList 的新值反映到界面上。
+// 目前整体组件都会刷新，但是只有banner使用到了BannerLst,所以需要使得只刷新banner组件
+// 状态管理
--- lib/pages/home/home_vm.dart
+++ lib/pages/home/home_vm.dart
@@ -1,16 +1,34 @@
 import 'package:code01/datas/home_banner_data.dart';
+import 'package:code01/datas/home_list_data.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 
-class HomeViewModel {
+class HomeViewModel with ChangeNotifier{
 
-  static Future<List<BannerItemData>?> getBanner() async {
-    Dio dio = Dio();
+  List<BannerItemData>? bannerList = [];
+  List<HomeItemData>? homeArticleList = [];
+  Dio dio = Dio();
+  void initDio(){
     dio.options = BaseOptions(baseUrl: 'https://www.wanandroid.com',
-        connectTimeout: Duration(seconds: 30),receiveTimeout: Duration(seconds: 30),
+      connectTimeout: Duration(seconds: 30),receiveTimeout: Duration(seconds: 30),
       sendTimeout: Duration(seconds: 30),
     );
+  }
+
+  Future getBanner() async {
     Response res = await dio.get('/banner/json');
     HomeBannerData bannerData = HomeBannerData.fromJson(res.data);
-    return bannerData.data;
+    bannerList = bannerData.data;
+    print(bannerList.toString());
+    notifyListeners();
+  }
+
+// 获取文章列表
+  Future getArticle() async {
+    Response response = await dio.get('/article/list/1/json');
+    HomeData homeData = HomeData.fromJson(response.data);
+    homeArticleList = homeData.data?.datas ?? [];
+    print(homeArticleList.toString());
+    notifyListeners();
   }
 }
